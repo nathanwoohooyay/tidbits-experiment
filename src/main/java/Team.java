96b5a3e8d9a248518eec6a670e5efaba9fbf18fa
Patrick Menninger <patrick.menninger@gmail.com>
2026-09-15 19:32:03 +0000

    Added Team.java which contains fields and methods for teams

---
+++ src/main/java/Team.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+
+    private String name;
+    private double budget;
+    private double budgetUsage;
+    private List<Game> schedule;
+    private List<Player> players;
+
+    public Team(String name, double budget) {
+        this.name = name;
+        this.budget = budget;
+        this.budgetUsage = 0;
+
+        schedule = new ArrayList<>();
+        players = new ArrayList<>();
+    }
+
+    public Team(String name, double budget, double budgetUsage, List<Game> games, List<Player> players) {
+        this.name = name;
+        this.budget = budget;
+        this.budgetUsage = budgetUsage;
+
+        this.schedule = games;
+        this.players = players;
+    }
+
+    public boolean canAfford(double amount) {
+        return budgetUsage + amount <= budget;
+    }
+
+    public void addPlayer(Player player) {
+        if (players.contains(player)) {
+            throw new IllegalArgumentException("Player already on team");
+        }
+
+        if (this.canAfford(player.salary)) {
+            players.add(player);
+            budget += budgetUsage;
+
+        } else {
+            throw new IllegalArgumentException("Can't afford player");
+        }
+
+    }
+
+    public void removePlayer(Player player) {
+        if (!players.contains(player)) {
+            throw new IllegalArgumentException("Player not on team");
+        }
+
+        players.remove(player);
+        budgetUsage -= player.salary;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getBudgetUsage() {
+        return budgetUsage;
+    }
+
+    public void setBudgetUsage(double budgetUsage) {
+        this.budgetUsage = budgetUsage;
+    }
+}
